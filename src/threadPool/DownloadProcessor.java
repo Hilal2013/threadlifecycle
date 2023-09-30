5b3970ef806d11ea8482beb73a968862205735fc
Hilal2013 <hilalozkul@gmail.com>
2023-09-30 17:10:36 -0400

    DownloadProcessor

--- src/threadPool/DownloadProcessor.java
+++ src/threadPool/DownloadProcessor.java
@@ -9,13 +9,12 @@ public class DownloadProcessor implements Runnable{
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName()+ "has started download the file");
-        try{
-          Thread.sleep(2500);
-        }catch(InterruptedException e){
-            new RuntimeException(e);
+        System.out.println(Thread.currentThread().getName() + "has started download the file: " +fileName );
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        System.out.println(Thread.currentThread().getName()+ "download finished");
-
+        System.out.println(Thread.currentThread().getName() + "download is finished. the file: " +fileName );
     }
 }
